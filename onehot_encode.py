ea207bff31db7c4e275ad5da8a335e10e48a410c
bnalavex <bhaskarx.nalaveli@intel.com>
2023-12-22 19:21:12 +0530

    adding one hot encoding code

---
+++ onehot_encode.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def ohe_m(text):
+    tokens = set(text.lower().split())
+    length = len(tokens)
+    index_map = {x: index for x, index in zip(tokens, range(length))}
+    ohe_matrix = []
+
+    for token in tokens:
+        ohe = np.zeros(length)
+        ohe[index_map[token]] = 1
+        print(token, ohe)
+        ohe_matrix.append(ohe)
